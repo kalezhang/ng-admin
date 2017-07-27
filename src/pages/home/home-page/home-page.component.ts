4377f478fde8c1533d36ed7b9abaf8ad62ef8bb3
Zhang <kale.jinyou.zhang@accenture.com>
2017-07-27 18:20:27 +0800

    feat: order and ts test1

--- src/pages/home/home-page/home-page.component.ts
+++ src/pages/home/home-page/home-page.component.ts
@@ -9,56 +9,85 @@ import { Meta, Title } from '@angular/platform-browser';
 export class HomePageComponent implements OnInit {
   users:Array<any> = [
     {"index":19,
-    "name":"刘雨飏",
-      "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
+    "prodtype":"家电",
+      "prodname":"电吹风",
+      "price":"189",
+      "brand":"飞利浦",
+      "zhishu":"5",
+      "pinglun":100,
+      "monthcnt":99},
       {"index":7,
-      "name":"小明",
-      "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
+      "prodtype":"零食",
+      "prodname":"薯片",
+      "price":"4.9",
+      "brand":"未知",
+      "zhishu":5,
+      "pinglun":20000,
+      "monthcnt":22266},
       {"index":14,
-      "name":"Chunk",
-      "sex":"Male",
-      "name_en":"Chunk",
-      "github":"chunk",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76}
+      "prodtype":"日用品",
+      "prodname":"纸抽",
+      "price":"50",
+      "brand":"清风",
+      "zhishu":4,
+      "pinglun":2222,
+      "monthcnt":66666}
   ]
 deleteLast(){
   this.users.pop()
 }
 saveNewUser(){
-  this.users.push({
-     "index":666,
-      "name":"New User",
-      "sex":"Male",
-      "name_en":"new_user",
-      "github":"new_user",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76
-  })
+  this.users.push({"index":19,
+    "prodtype":"家电2",
+      "prodname":"电吹风",
+      "price":"189",
+      "brand":"飞利浦",
+      "zhishu":"5",
+      "pinglun":100,
+      "monthcnt":99})
 }
   sortByAsccending(){
     // 参考MDN Array操作的API文档 Array相关方法方法
+    this.users.sort(function (a, b) {
+        if (a.index > b.index) {
+          return 1;
+        }
+        if (a.index < b.index) {
+          return -1;
+        }
+        // a 必须等于 b
+        return 0;
+      });
   }
   sortByDesccending(){
+    this.users.sort(function (a, b) {
+        if (a.index < b.index) {
+          return 1;
+        }
+        if (a.index > b.index) {
+          return -1;
+        }
+        // a 必须等于 b
+        return 0;
+      });
     // 参考MDN Array操作的API文档 Array相关方法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
   }
   sortByRadom(){
+    this.users.sort(function (a, b) {
+        if (Math.random() > 0.5) {
+          return 1;
+        }
+        if (Math.random() < 0.5) {
+          return -1;
+        }
+        // a 必须等于 b
+        return 0;
+      });
     // 参考MDN Array操作的API文档 Math相关方法
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
   }
+
   constructor(meta: Meta, title: Title) {
     title.setTitle('My Home Page');
 
