fd383f92c885192f508a66cec95efdf469a3a681
Zhang <kale.jinyou.zhang@accenture.com>
2017-07-28 16:30:06 +0800

    feat:temp change

--- src/pages/home/home-page/home-page.component.ts
+++ src/pages/home/home-page/home-page.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Meta,
+  Title
+} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-page',
@@ -7,83 +13,90 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  users:Array<any> = [
-    {"index":19,
-    "prodtype":"家电",
-      "prodname":"电吹风",
-      "price":"189",
-      "brand":"飞利浦",
-      "zhishu":"5",
-      "pinglun":100,
-      "monthcnt":99},
-      {"index":7,
-      "prodtype":"零食",
-      "prodname":"薯片",
-      "price":"4.9",
-      "brand":"未知",
-      "zhishu":5,
-      "pinglun":20000,
-      "monthcnt":22266},
-      {"index":14,
-      "prodtype":"日用品",
-      "prodname":"纸抽",
-      "price":"50",
-      "brand":"清风",
-      "zhishu":4,
-      "pinglun":2222,
-      "monthcnt":66666}
+  users: Array < any > = [{
+      "index": 19,
+      "prodtype": "家电",
+      "prodname": "电吹风",
+      "price": "189",
+      "brand": "飞利浦",
+      "zhishu": "5",
+      "pinglun": 100,
+      "monthcnt": 99
+    },
+    {
+      "index": 7,
+      "prodtype": "零食",
+      "prodname": "薯片",
+      "price": "4.9",
+      "brand": "未知",
+      "zhishu": 5,
+      "pinglun": 20000,
+      "monthcnt": 22266
+    },
+    {
+      "index": 14,
+      "prodtype": "日用品",
+      "prodname": "纸抽",
+      "price": "50",
+      "brand": "清风",
+      "zhishu": 4,
+      "pinglun": 2222,
+      "monthcnt": 66666
+    }
   ]
-deleteLast(){
-  this.users.pop()
-}
-saveNewUser(){
-  this.users.push({"index":Math.round(Math.random() * 100),
-    "prodtype":"新家电",
-      "prodname":"电吹风",
-      "price":Math.round(Math.random() * 100),
-      "brand":"飞利浦",
-      "zhishu":"5",
-      "pinglun":100,
-      "monthcnt":99})
-}
-  sortByAsccending(){
+  deleteLast() {
+    this.users.pop()
+  }
+  saveNewUser() {
+    this.users.push({
+      "index": Math.round(Math.random() * 100),
+      "prodtype": "新家电",
+      "prodname": "电吹风",
+      "price": Math.round(Math.random() * 100),
+      "brand": "飞利浦",
+      "zhishu": "5",
+      "pinglun": 100,
+      "monthcnt": 99
+    })
+  }
+  sortByAsccending() {
     // 参考MDN Array操作的API文档 Array相关方法方法
     this.users.sort(function (a, b) {
-        if (a.index > b.index) {
-          return 1;
-        }
-        if (a.index < b.index) {
-          return -1;
-        }
-        // a 必须等于 b
-        return 0;
-      });
+      if (a.index > b.index) {
+        return 1;
+      }
+      if (a.index < b.index) {
+        return -1;
+      }
+      // a 必须等于 b
+      return 0;
+    });
   }
-  sortByDesccending(){
+  sortByDesccending() {
     this.users.sort(function (a, b) {
-        if (a.index < b.index) {
-          return 1;
-        }
-        if (a.index > b.index) {
-          return -1;
-        }
-        // a 必须等于 b
-        return 0;
-      });
+      if (a.index < b.index) {
+        return 1;
+      }
+      if (a.index > b.index) {
+        return -1;
+      }
+      // a 必须等于 b
+      return 0;
+    });
     // 参考MDN Array操作的API文档 Array相关方法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
   }
-  sortByRadom(){
+  sortByRadom() {
     this.users.sort(function (a, b) {
-        if (Math.random() > 0.5) {
-          return 1;
-        }
-        if (Math.random() < 0.5) {
-          return -1;
-        }
-        // a 必须等于 b
-        return 0;
-      });
+      if (Math.random() > 0.5) {
+        return 1;
+      }
+      if (Math.random() < 0.5) {
+        return -1;
+      }
+      // a 必须等于 b
+      return 0;
+    });
     // 参考MDN Array操作的API文档 Math相关方法
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
   }
@@ -91,20 +104,21 @@ saveNewUser(){
   constructor(meta: Meta, title: Title) {
     title.setTitle('My Home Page');
 
-    meta.addTags([ 
-      {
-        name: 'author', content: 'eddic'
+    meta.addTags([{
+        name: 'author',
+        content: 'eddic'
       },
       {
-        name: 'keywords', content: 'angular 4 tutorial, angular seo'
+        name: 'keywords',
+        content: 'angular 4 tutorial, angular seo'
       },
       {
-        name: 'description', content: 'This is my great description.'
+        name: 'description',
+        content: 'This is my great description.'
       },
     ])
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
